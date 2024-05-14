93ca882aef1871399035fb4b1306830ec12a9aad
itsmuiz <pvt.muiz1012@gmail.com>
2024-05-14 02:54:06 -0700

    nav of pc web

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 console.log('start');
 
-let li = document.querySelectorAll('#drop');
+let down = document.querySelector('.down');
+let drop = document.querySelector('.drop');
+
+down.addEventListener('click', ()=>{
+    if (drop.style.display === 'none') {
+        drop.style.display = 'flex';
+    } else {
+        drop.style.display = 'none';
+    }
+});
+
 
